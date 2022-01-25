eb0e1af396810a3610a01369a57c55b4525b4927
chin <1277180540@qq.com>
2022-01-25 23:36:13 +0800

    i cannot dp

---
+++ 232-用栈实现队列.js
@@ -0,0 +1,55 @@
+var MyQueue = function () {
+  // 创建数组，模拟两个栈
+  this.stack1 = [];
+  this.stack2 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (!this.stack2.length) {
+    // 只有当stack2为空的时候
+    while (this.stack1.length) {
+      this.stack2.push(this.stack1.pop());
+    }
+  }
+  return this.stack2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (!this.stack2.length) {
+    // 只有当stack2为空的时候
+    while (this.stack1.length) {
+      this.stack2.push(this.stack1.pop());
+    }
+  }
+  return this.stack2[this.stack2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return !this.stack1.length && !this.stack2.length;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
