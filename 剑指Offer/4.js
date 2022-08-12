b02b2412dc14c1315f228ff49ab8668d9bf218f9
chin <1277180540@qq.com>
2022-08-12 19:18:10 +0800

    add

---
+++ 剑指Offer/4.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// var findNumberIn2DArray = function (matrix, target) {
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       if (matrix[i][j] === target) return true
+//     }
+//   }
+
+//   return false
+// }
+
+// console.log(
+//   findNumberIn2DArray(
+//     [
+//       [1, 4, 7, 11, 15],
+//       [2, 5, 8, 12, 19],
+//       [3, 6, 9, 16, 22],
+//       [10, 13, 14, 17, 24],
+//       [18, 21, 23, 26, 30]
+//     ],
+//     5
+//   )
+// )
+
+const findNumberIn2DArray = (matrix, target) => {
+  
+}
